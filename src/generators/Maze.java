b6707decb56c68853cb2ccf1834e3dc0213e9daa
yjhang3 <yoorijhang@gatech.edu>
2021-03-13 02:41:04 -0500

    add to maze

--- src/generators/Maze.java
+++ src/generators/Maze.java
@@ -29,13 +29,18 @@ public class Maze {
         Node nodeFive = new Node(8, false);
         Node nodeSix = new Node(6, false);
         Node nodeSeven = new Node(4, false);
-        Node nodeEight = new Node(8, false);
+        Node nodeEight = new Node(5, false);
         Node nodeNine = new Node(6, false);
         Node nodeTen = new Node(5, false);
         Node nodeEleven = new Node(5, false);
         Node nodeTwelve = new Node(7, false);
         Node nodeThirteen = new Node(6, false);
         Node nodeFourteen = new Node(10, false);
+        Node nodeFifteen = new Node(7, false);
+        Node nodeSixteen = new Node(9, false);
+        Node nodeSeventeen = new Node(8, false);
+        Node nodeEighteen = new Node(1, false);
+        Node nodeExit = new Node(12, true);
 
         startNode.top = nodeOne;
         startNode.right = nodeTwo;
@@ -69,6 +74,7 @@ public class Maze {
 
         nodeEight.right = nodeTen;
         nodeEight.bottom = nodeOne;
+        nodeEight.left = nodeFifteen;
 
         nodeNine.top = nodeTen;
         nodeNine.left = nodeTwo;
@@ -88,6 +94,46 @@ public class Maze {
         nodeThirteen.top = nodeFourteen;
 
         nodeFourteen.bottom = nodeThirteen;
+
+        nodeFifteen.top = nodeSixteen;
+        nodeFifteen.right = nodeEight;
+
+        nodeSixteen.left = nodeSeventeen;
+        nodeSixteen.bottom = nodeFifteen;
+
+        nodeSeventeen.right = nodeSixteen;
+        nodeSeventeen.bottom = nodeEighteen;
+
+        nodeEighteen.top = nodeSeventeen;
+
+        Random random = new Random();
+        int randExitRoom = random.nextInt(2);   // exit room can be connected to 14 or 18
+        int randExitPath = random.nextInt(3);
+
+        if (randExitRoom == 0) {    // room 18 is connected to exit ... right, bottom, left possible
+            if (randExitPath == 0) {    // right exit
+                nodeEighteen.right = nodeExit;
+                nodeExit.left = nodeEighteen;
+            } else if (randExitPath == 1) { // bottom exit
+                nodeEighteen.bottom = nodeExit;
+                nodeExit.top = nodeEighteen;
+            } else {    // left exit
+                nodeEighteen.left = nodeExit;
+                nodeExit.right = nodeEighteen;
+            }
+        } else {    // room 14 is connected to exit ... top, right, left possible
+            if (randExitPath == 0) {    // right exit
+                nodeFourteen.right = nodeExit;
+                nodeExit.left = nodeFourteen;
+            } else if (randExitPath == 1) { // top exit
+                nodeFourteen.top = nodeExit;
+                nodeExit.bottom = nodeFourteen;
+            } else {    // left exit
+                nodeFourteen.left = nodeExit;
+                nodeExit.right = nodeFourteen;
+            }
+        }
+
     }
 
 }
