9b70aa6a0fe69d877f5ecf2abbdb610e321d6cb3
ebang8 <ebang8@gatech.edu>
2021-03-13 16:04:27 -0500

    Add exit room alert, fix minor issues in scene changes

--- src/generators/Maze.java
+++ src/generators/Maze.java
@@ -7,7 +7,7 @@ import javafx.scene.layout.*;
 import java.util.Random;
 
 public class Maze {
-
+    private static Random random;
     public class Node {
         int roomIdentifier;
         Node top;
@@ -16,8 +16,9 @@ public class Maze {
         Node right;
         Image imageBkgd;
         boolean isExit;
+        int roomNum;
 
-        Node (int roomIdentifier, boolean isExit) {
+        Node (int roomIdentifier, boolean isExit, int roomNum) {
             this.roomIdentifier = roomIdentifier;
             top = null;
             bottom = null;
@@ -25,6 +26,7 @@ public class Maze {
             right = null;
             imageBkgd = null;
             this.isExit = isExit;
+            this.roomNum = roomNum;
         }
 
         public Node getRight() {
@@ -45,29 +47,35 @@ public class Maze {
         public boolean getIsExit() {
             return isExit;
         }
+        public int getRoomIdentifier() {
+            return roomIdentifier;
+        }
+        public int getRoomNum() {
+            return roomNum;
+        }
     }
     private static Node curr;
     public Maze() {    // constructor
-        Node startNode = new Node(1, false);
-        Node nodeOne = new Node(4, false);
-        Node nodeTwo = new Node(3, false);
-        Node nodeThree = new Node(5, false);
-        Node nodeFour = new Node(2, false);
-        Node nodeFive = new Node(8, false);
-        Node nodeSix = new Node(6, false);
-        Node nodeSeven = new Node(4, false);
-        Node nodeEight = new Node(5, false);
-        Node nodeNine = new Node(6, false);
-        Node nodeTen = new Node(5, false);
-        Node nodeEleven = new Node(5, false);
-        Node nodeTwelve = new Node(7, false);
-        Node nodeThirteen = new Node(6, false);
-        Node nodeFourteen = new Node(10, false);
-        Node nodeFifteen = new Node(7, false);
-        Node nodeSixteen = new Node(9, false);
-        Node nodeSeventeen = new Node(8, false);
-        Node nodeEighteen = new Node(1, false);
-        Node nodeExit = new Node(12, true);
+        Node startNode = new Node(1, false,0);
+        Node nodeOne = new Node(4, false, 1);
+        Node nodeTwo = new Node(3, false, 2);
+        Node nodeThree = new Node(5, false, 3);
+        Node nodeFour = new Node(2, false, 4);
+        Node nodeFive = new Node(8, false, 5);
+        Node nodeSix = new Node(6, false, 6);
+        Node nodeSeven = new Node(4, false, 7);
+        Node nodeEight = new Node(5, false, 8);
+        Node nodeNine = new Node(6, false, 9);
+        Node nodeTen = new Node(5, false, 10);
+        Node nodeEleven = new Node(5, false, 11);
+        Node nodeTwelve = new Node(7, false, 12);
+        Node nodeThirteen = new Node(6, false, 13);
+        Node nodeFourteen = new Node(1, false, 14);
+        Node nodeFifteen = new Node(7, false, 15);
+        Node nodeSixteen = new Node(9, false, 16);
+        Node nodeSeventeen = new Node(8, false, 17);
+        Node nodeEighteen = new Node(1, false, 18);
+        Node nodeExit = new Node(12, true, -1);
 
         startNode.top = nodeOne;
         startNode.right = nodeTwo;
@@ -106,6 +114,7 @@ public class Maze {
 
         nodeSeven.top = nodeFive;
         nodeSeven.bottom = nodeEleven;
+        nodeSeven.right = nodeSix;
         nodeSeven.imageBkgd = new Image("/mazeroom7.png");
 
         nodeEight.right = nodeTen;
@@ -120,7 +129,7 @@ public class Maze {
         nodeTen.left = nodeEight;
         nodeTen.right = nodeEleven;
         nodeTen.bottom = nodeNine;
-        nodeTen.imageBkgd = new Image("/mazeroom8.png");
+        nodeTen.imageBkgd = new Image("/mazeroom10.png");
 
         nodeEleven.right = nodeTen;
         nodeEleven.bottom = nodeTwelve;
@@ -153,33 +162,55 @@ public class Maze {
         nodeEighteen.top = nodeSeventeen;
         nodeEighteen.imageBkgd = new Image("/mazeroom18.png");
 
-        Random random = new Random();
-        int randExitRoom = random.nextInt(2);   // exit room can be connected to 14 or 18
-        int randExitPath = random.nextInt(3);
-
-        if (randExitRoom == 0) {    // room 18 is connected to exit ... right, bottom, left possible
-            if (randExitPath == 0) {    // right exit
+        random = new Random();
+        int randExitRoom = random.nextInt(6);   // exit room can be connected to 14 or 18
+            // room 18 is connected to exit ... right, bottom, left possible
+            if (randExitRoom == 0) {    // right exit
                 nodeEighteen.right = nodeExit;
                 nodeExit.left = nodeEighteen;
-            } else if (randExitPath == 1) { // bottom exit
+            } else if (randExitRoom == 1) { // bottom exit
                 nodeEighteen.bottom = nodeExit;
                 nodeExit.top = nodeEighteen;
-            } else {    // left exit
+            } else if (randExitRoom == 2){    // left exit
                 nodeEighteen.left = nodeExit;
                 nodeExit.right = nodeEighteen;
-            }
-        } else {    // room 14 is connected to exit ... top, right, left possible
-            if (randExitPath == 0) {    // right exit
+                // room 14 is connected to exit ... top, right, left possible
+            } else if (randExitRoom == 3) {    // right exit
                 nodeFourteen.right = nodeExit;
                 nodeExit.left = nodeFourteen;
-            } else if (randExitPath == 1) { // top exit
+            } else if (randExitRoom == 4) { // top exit
                 nodeFourteen.top = nodeExit;
                 nodeExit.bottom = nodeFourteen;
             } else {    // left exit
                 nodeFourteen.left = nodeExit;
                 nodeExit.right = nodeFourteen;
             }
-        }
+
+//        int randExitPath = random.nextInt(3);
+
+//        if (randExitRoom == 0) {    // room 18 is connected to exit ... right, bottom, left possible
+//            if (randExitPath == 0) {    // right exit
+//                nodeEighteen.right = nodeExit;
+//                nodeExit.left = nodeEighteen;
+//            } else if (randExitPath == 1) { // bottom exit
+//                nodeEighteen.bottom = nodeExit;
+//                nodeExit.top = nodeEighteen;
+//            } else {    // left exit
+//                nodeEighteen.left = nodeExit;
+//                nodeExit.right = nodeEighteen;
+//            }
+//        } else {    // room 14 is connected to exit ... top, right, left possible
+//            if (randExitPath == 0) {    // right exit
+//                nodeFourteen.right = nodeExit;
+//                nodeExit.left = nodeFourteen;
+//            } else if (randExitPath == 1) { // top exit
+//                nodeFourteen.top = nodeExit;
+//                nodeExit.bottom = nodeFourteen;
+//            } else {    // left exit
+//                nodeFourteen.left = nodeExit;
+//                nodeExit.right = nodeFourteen;
+//            }
+//        }
         curr = startNode;
     }
     //getters
