3f7b020735ff4a6f68ee03f16e31e7a646cd0ad4
wdavenport6 <wdavenport6@gatech.edu>
2021-03-22 12:24:04 -0400

    replace switch with if for player weapon

--- src/scenes/Player.java
+++ src/scenes/Player.java
@@ -1,24 +1,18 @@
 package scenes;
 
-import static scenes.InitializeConfigScreen.getCurrWeaponList;
+import static scenes.InitializeConfigScreen.*;
 
 public class Player {
     private int player_Health;
     private int player_Damage;
     public Player(int player_Health) {
         this.player_Health=player_Health;
-        switch (getCurrWeaponList()[0]) {
-            case "dagger":
-                this.player_Damage = 8;
-                break;
-            case "sword":
-                this.player_Damage = 10;
-                break;
-            case "greatsword":
-                this.player_Damage = 12;
-                break;
-            default:
-                this.player_Damage = 8;
+        if (getCurrWeaponList()[0].equals(getWeapon1())) {
+            this.player_Damage = 8;
+        } else if (getCurrWeaponList()[0].equals(getWeapon2())) {
+            this.player_Damage = 10;
+        } else {
+            this.player_Damage = 12;
         }
 
     }
