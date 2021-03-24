d3d9c959b21f134fc1a8d31e6ead151ed1748cb7
yjhang3 <yoorijhang@gatech.edu>
2021-03-24 04:21:10 -0400

    add isVisited to track visited rooms

--- src/scenes/InitialGameScreen.java
+++ src/scenes/InitialGameScreen.java
@@ -1,9 +1,7 @@
 package scenes;
 
 import generators.Maze;
-import javafx.event.EventHandler;
 import javafx.geometry.Pos;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -13,6 +11,8 @@ import javafx.scene.control.*;
 import generators.Maze.*;
 
 
+import static generators.Maze.getPlayer;
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
 import static scenes.InitializeConfigScreen.getGameDifficulty;
 import static javafx.application.Application.launch;
 
@@ -23,17 +23,23 @@ public class InitialGameScreen {
     private static Button exitRight;
     private static Button exitTop;
     private static Button exitBottom;
+    private static Button attackMonster;
     private static Label money;
     private static Label exitNotif;
     private static Maze maze;
     private static Node curr;
+    private static BorderPane root;
+
+    public static Node getCurr() {
+        return curr;
+    }
 
     public static Label getMoney() {
         return money;
     }
 
     public static void setMoney(Label money) {
-        InitialGameScreen.money = money;
+        scenes.InitialGameScreen.money = money;
     }
 
     public static Scene start(Stage primaryStage) {
@@ -68,11 +74,12 @@ public class InitialGameScreen {
         exitNotif.setAlignment(Pos.CENTER);
 
         // exit buttons
-        BorderPane root = new BorderPane();
+        root = new BorderPane();
         exitLeft = new Button("Exit Left");
         exitRight = new Button("Exit Right");
         exitTop = new Button("Exit Top");
         exitBottom = new Button("Exit Bottom");
+        attackMonster = new Button("Attack!");
         //setting start room page
         curr = Maze.getCurr();
 
@@ -100,6 +107,14 @@ public class InitialGameScreen {
         holdB.setSpacing(10);
         root.setBottom(holdB);
 
+        HBox holdM = new HBox();
+        holdM.getChildren().add(attackMonster);
+        holdM.setAlignment(Pos.CENTER);
+        holdM.setSpacing(10);
+        root.setCenter(holdM);
+
+
+
         Image imageBkgd = curr.getImageBkgd();
         BackgroundImage bkgdSettings = new BackgroundImage(imageBkgd, BackgroundRepeat.NO_REPEAT,
                 BackgroundRepeat.NO_REPEAT,
@@ -108,6 +123,39 @@ public class InitialGameScreen {
         Background background = new Background(bkgdSettings);
         root.setBackground(background);
 
+        setExitLeftAction();
+        setExitRightAction();
+        setExitTopAction();
+        setExitBottomAction();
+        setAttackMonsterAction();
+
+        // final panes and showing scene
+        primaryStage.setTitle("DungeonCrawler");
+        primaryStage.setScene(new Scene(root, bkgdWidth, bkgdHeight));
+        root.getChildren().addAll(hBox, vBox, pane, centerText);
+        return primaryStage.getScene();
+    }
+    public static void setAttackMonsterAction() {
+        attackMonster.setOnAction(e -> {
+            int playerHealth = getPlayer().getPlayer_Health();
+            int monsterDamage = curr.getMonster().getMonsterDamage();
+            int playerDamge= getPlayer().getPlayer_Damage();
+            int monsterHealth = curr.getMonster().getMonsterHealth();
+            if(curr.getMonster() instanceof MonsterBlue) {
+                System.out.println("BlueMonster");
+            }
+            getPlayer().setPlayer_Health(playerHealth - monsterDamage);
+            curr.getMonster().setMonsterHealth(monsterHealth - playerDamge);
+            if (curr.getMonster().getMonsterHealth() <= 0) {
+                curr.getMonster().setMonsterIsDead(true);
+            }
+            if (curr.getMonster().getMonsterIsDead()) {
+                System.out.println("MonsterIsDead");
+                attackMonster.setVisible(false);
+            }
+        });
+    }
+    public static void setExitLeftAction() {
         exitLeft.setOnAction(e -> {
             if (curr.getRoomNum() == 14 || curr.getRoomNum() == 18) {
                 if (curr.getLeft() == null) {
@@ -116,7 +164,9 @@ public class InitialGameScreen {
                     alertExit.show();
                 }
             }
-            if (curr.getLeft() != null) {
+            if (curr.getLeft() != null && (curr.getMonster().getMonsterIsDead() ||
+                    (!curr.getMonster().getMonsterIsDead() && curr.getLeft().getIsVisted()))) {
+                curr.setIsVisted(true);
                 curr = curr.getLeft();
                 Image imgBkgd = curr.getImageBkgd();
                 BackgroundImage bkgdSet = new BackgroundImage(imgBkgd, BackgroundRepeat.NO_REPEAT,
@@ -126,20 +176,28 @@ public class InitialGameScreen {
                 Background bkgd = new Background(bkgdSet);
                 root.setBackground(bkgd);
                 //button visibility
-                exitTop.setVisible(curr.getRoomIdentifier() == 1 || curr.getRoomIdentifier() == 2 || curr.getRoomIdentifier() == 3
-                        || curr.getRoomIdentifier() == 4 || curr.getRoomIdentifier() == 6
-                        || curr.getRoomIdentifier() == 7 || curr.getRoomIdentifier() == 10);
-                exitRight.setVisible(curr.getRoomIdentifier() == 1 || curr.getRoomIdentifier() == 3 || curr.getRoomIdentifier() == 4
-                        || curr.getRoomIdentifier() == 5 || curr.getRoomIdentifier() == 7
-                        || curr.getRoomIdentifier() == 8 || curr.getRoomIdentifier() == 11);
-                exitBottom.setVisible(curr.getRoomIdentifier() == 1 || curr.getRoomIdentifier() == 2 || curr.getRoomIdentifier() == 4
-                        || curr.getRoomIdentifier() == 5 || curr.getRoomIdentifier() == 8
-                        || curr.getRoomIdentifier() == 9 || curr.getRoomIdentifier() == 10);
-                exitLeft.setVisible(curr.getRoomIdentifier() == 1 || curr.getRoomIdentifier() == 2 || curr.getRoomIdentifier() == 3
-                        || curr.getRoomIdentifier() == 5 || curr.getRoomIdentifier() == 6
-                        || curr.getRoomIdentifier() == 9 || curr.getRoomIdentifier() == 11);
+                exitTop.setVisible(curr.getRoomIdentifier() == 1 || curr.getRoomIdentifier() == 2
+                        || curr.getRoomIdentifier() == 3 || curr.getRoomIdentifier() == 4
+                        || curr.getRoomIdentifier() == 6 || curr.getRoomIdentifier() == 7
+                        || curr.getRoomIdentifier() == 10);
+                exitRight.setVisible(curr.getRoomIdentifier() == 1 || curr.getRoomIdentifier() == 3
+                        || curr.getRoomIdentifier() == 4 || curr.getRoomIdentifier() == 5
+                        || curr.getRoomIdentifier() == 7 || curr.getRoomIdentifier() == 8
+                        || curr.getRoomIdentifier() == 11);
+                exitBottom.setVisible(curr.getRoomIdentifier() == 1 || curr.getRoomIdentifier() == 2
+                        || curr.getRoomIdentifier() == 4 || curr.getRoomIdentifier() == 5
+                        || curr.getRoomIdentifier() == 8 || curr.getRoomIdentifier() == 9
+                        || curr.getRoomIdentifier() == 10);
+                exitLeft.setVisible(curr.getRoomIdentifier() == 1 || curr.getRoomIdentifier() == 2
+                        || curr.getRoomIdentifier() == 3 || curr.getRoomIdentifier() == 5
+                        || curr.getRoomIdentifier() == 6 || curr.getRoomIdentifier() == 9
+                        || curr.getRoomIdentifier() == 11);
+                attackMonster.setVisible(!(curr.getMonster().monsterIsDead));
             }
         });
+    }
+
+    public static void setExitRightAction() {
         exitRight.setOnAction(e -> {
             if (curr.getRoomNum() == 14 || curr.getRoomNum() == 18) {
                 if (curr.getRight() == null) {
@@ -148,7 +206,9 @@ public class InitialGameScreen {
                     alertExit.show();
                 }
             }
-            if (curr.getRight() != null) {
+            if (curr.getRight() != null && (curr.getMonster().getMonsterIsDead() ||
+                    (!curr.getMonster().getMonsterIsDead() && curr.getRight().getIsVisted()))) {
+                curr.setIsVisted(true);
                 curr = curr.getRight();
                 Image imgBkgd = curr.getImageBkgd();
                 BackgroundImage bkgdSet = new BackgroundImage(imgBkgd, BackgroundRepeat.NO_REPEAT,
@@ -158,20 +218,32 @@ public class InitialGameScreen {
                 Background bkgd = new Background(bkgdSet);
                 root.setBackground(bkgd);
                 //button visibility
-                exitTop.setVisible(curr.getRoomIdentifier() == 1 || curr.getRoomIdentifier() == 2 || curr.getRoomIdentifier() == 3
-                        || curr.getRoomIdentifier() == 4 || curr.getRoomIdentifier() == 6
-                        || curr.getRoomIdentifier() == 7 || curr.getRoomIdentifier() == 10);
-                exitRight.setVisible(curr.getRoomIdentifier() == 1 || curr.getRoomIdentifier() == 3 || curr.getRoomIdentifier() == 4
-                        || curr.getRoomIdentifier() == 5 || curr.getRoomIdentifier() == 7
-                        || curr.getRoomIdentifier() == 8 || curr.getRoomIdentifier() == 11);
-                exitBottom.setVisible(curr.getRoomIdentifier() == 1 || curr.getRoomIdentifier() == 2 || curr.getRoomIdentifier() == 4
-                        || curr.getRoomIdentifier() == 5 || curr.getRoomIdentifier() == 8
-                        || curr.getRoomIdentifier() == 9 || curr.getRoomIdentifier() == 10);
-                exitLeft.setVisible(curr.getRoomIdentifier() == 1 || curr.getRoomIdentifier() == 2 || curr.getRoomIdentifier() == 3
-                        || curr.getRoomIdentifier() == 5 || curr.getRoomIdentifier() == 6
-                        || curr.getRoomIdentifier() == 9 || curr.getRoomIdentifier() == 11);
+                exitTop.setVisible(curr.getRoomIdentifier() == 1 || curr.getRoomIdentifier() == 2
+                        || curr.getRoomIdentifier() == 3 || curr.getRoomIdentifier() == 4
+                        || curr.getRoomIdentifier() == 6 || curr.getRoomIdentifier() == 7
+                        || curr.getRoomIdentifier() == 10);
+                exitRight.setVisible(curr.getRoomIdentifier() == 1 || curr.getRoomIdentifier() == 3
+                        || curr.getRoomIdentifier() == 4 || curr.getRoomIdentifier() == 5
+                        || curr.getRoomIdentifier() == 7 || curr.getRoomIdentifier() == 8
+                        || curr.getRoomIdentifier() == 11);
+                exitBottom.setVisible(curr.getRoomIdentifier() == 1 || curr.getRoomIdentifier() == 2
+                        || curr.getRoomIdentifier() == 4 || curr.getRoomIdentifier() == 5
+                        || curr.getRoomIdentifier() == 8 || curr.getRoomIdentifier() == 9
+                        || curr.getRoomIdentifier() == 10);
+                exitLeft.setVisible(curr.getRoomIdentifier() == 1 || curr.getRoomIdentifier() == 2
+                        || curr.getRoomIdentifier() == 3 || curr.getRoomIdentifier() == 5
+                        || curr.getRoomIdentifier() == 6 || curr.getRoomIdentifier() == 9
+                        || curr.getRoomIdentifier() == 11);
+                if(!(curr.getMonster().monsterIsDead)) {
+                    attackMonster.setVisible(true);
+                } else {
+                    attackMonster.setVisible(false);
+                }
             }
         });
+    }
+
+    public static void setExitTopAction() {
         exitTop.setOnAction(e -> {
             if (curr.getRoomNum() == 14) {
                 if (curr.getTop() == null) {
@@ -180,7 +252,9 @@ public class InitialGameScreen {
                     alertExit.show();
                 }
             }
-            if (curr.getTop() != null) {
+            if (curr.getTop() != null && (curr.getMonster().getMonsterIsDead() ||
+                    (!curr.getMonster().getMonsterIsDead() && curr.getTop().getIsVisted()))) {
+                curr.setIsVisted(true);
                 curr = curr.getTop();
                 Image imgBkgd = curr.getImageBkgd();
                 BackgroundImage bkgdSet = new BackgroundImage(imgBkgd, BackgroundRepeat.NO_REPEAT,
@@ -190,21 +264,33 @@ public class InitialGameScreen {
                 Background bkgd = new Background(bkgdSet);
                 root.setBackground(bkgd);
                 //button visibility
-                exitTop.setVisible(curr.getRoomIdentifier() == 1 || curr.getRoomIdentifier() == 2 || curr.getRoomIdentifier() == 3
-                        || curr.getRoomIdentifier() == 4 || curr.getRoomIdentifier() == 6
-                        || curr.getRoomIdentifier() == 7 || curr.getRoomIdentifier() == 10);
-                exitRight.setVisible(curr.getRoomIdentifier() == 1 || curr.getRoomIdentifier() == 3 || curr.getRoomIdentifier() == 4
-                        || curr.getRoomIdentifier() == 5 || curr.getRoomIdentifier() == 7
-                        || curr.getRoomIdentifier() == 8 || curr.getRoomIdentifier() == 11);
-                exitBottom.setVisible(curr.getRoomIdentifier() == 1 || curr.getRoomIdentifier() == 2 || curr.getRoomIdentifier() == 4
-                        || curr.getRoomIdentifier() == 5 || curr.getRoomIdentifier() == 8
-                        || curr.getRoomIdentifier() == 9 || curr.getRoomIdentifier() == 10);
-                exitLeft.setVisible(curr.getRoomIdentifier() == 1 || curr.getRoomIdentifier() == 2 || curr.getRoomIdentifier() == 3
-                        || curr.getRoomIdentifier() == 5 || curr.getRoomIdentifier() == 6
-                        || curr.getRoomIdentifier() == 9 || curr.getRoomIdentifier() == 11);
+                exitTop.setVisible(curr.getRoomIdentifier() == 1 || curr.getRoomIdentifier() == 2
+                        || curr.getRoomIdentifier() == 3 || curr.getRoomIdentifier() == 4
+                        || curr.getRoomIdentifier() == 6 || curr.getRoomIdentifier() == 7
+                        || curr.getRoomIdentifier() == 10);
+                exitRight.setVisible(curr.getRoomIdentifier() == 1 || curr.getRoomIdentifier() == 3
+                        || curr.getRoomIdentifier() == 4 || curr.getRoomIdentifier() == 5
+                        || curr.getRoomIdentifier() == 7 || curr.getRoomIdentifier() == 8
+                        || curr.getRoomIdentifier() == 11);
+                exitBottom.setVisible(curr.getRoomIdentifier() == 1 || curr.getRoomIdentifier() == 2
+                        || curr.getRoomIdentifier() == 4 || curr.getRoomIdentifier() == 5
+                        || curr.getRoomIdentifier() == 8 || curr.getRoomIdentifier() == 9
+                        || curr.getRoomIdentifier() == 10);
+                exitLeft.setVisible(curr.getRoomIdentifier() == 1 || curr.getRoomIdentifier() == 2
+                        || curr.getRoomIdentifier() == 3 || curr.getRoomIdentifier() == 5
+                        || curr.getRoomIdentifier() == 6 || curr.getRoomIdentifier() == 9
+                        || curr.getRoomIdentifier() == 11);
+                if(!(curr.getMonster().monsterIsDead)) {
+                    attackMonster.setVisible(true);
+                } else {
+                    attackMonster.setVisible(false);
+                }
 
             }
         });
+    }
+
+    public static void setExitBottomAction() {
         exitBottom.setOnAction(e -> {
             if (curr.getRoomNum() == 18) {
                 if (curr.getBottom() == null) {
@@ -213,7 +299,9 @@ public class InitialGameScreen {
                     alertExit.show();
                 }
             }
-            if (curr.getBottom() != null) {
+            if (curr.getBottom() != null && (curr.getMonster().getMonsterIsDead() ||
+                    (!curr.getMonster().getMonsterIsDead() && curr.getBottom().getIsVisted()))) {
+                curr.setIsVisted(true);
                 curr = curr.getBottom();
                 Image imgBkgd = curr.getImageBkgd();
                 BackgroundImage bkgdSet = new BackgroundImage(imgBkgd, BackgroundRepeat.NO_REPEAT,
@@ -223,26 +311,29 @@ public class InitialGameScreen {
                 Background bkgd = new Background(bkgdSet);
                 root.setBackground(bkgd);
                 //button visibility
-                exitTop.setVisible(curr.getRoomIdentifier() == 1 || curr.getRoomIdentifier() == 2 || curr.getRoomIdentifier() == 3
-                        || curr.getRoomIdentifier() == 4 || curr.getRoomIdentifier() == 6
-                        || curr.getRoomIdentifier() == 7 || curr.getRoomIdentifier() == 10);
-                exitRight.setVisible(curr.getRoomIdentifier() == 1 || curr.getRoomIdentifier() == 3 || curr.getRoomIdentifier() == 4
-                        || curr.getRoomIdentifier() == 5 || curr.getRoomIdentifier() == 7
-                        || curr.getRoomIdentifier() == 8 || curr.getRoomIdentifier() == 11);
-                exitBottom.setVisible(curr.getRoomIdentifier() == 1 || curr.getRoomIdentifier() == 2 || curr.getRoomIdentifier() == 4
-                        || curr.getRoomIdentifier() == 5 || curr.getRoomIdentifier() == 8
-                        || curr.getRoomIdentifier() == 9 || curr.getRoomIdentifier() == 10);
-                exitLeft.setVisible(curr.getRoomIdentifier() == 1 || curr.getRoomIdentifier() == 2 || curr.getRoomIdentifier() == 3
-                        || curr.getRoomIdentifier() == 5 || curr.getRoomIdentifier() == 6
-                        || curr.getRoomIdentifier() == 9 || curr.getRoomIdentifier() == 11);
+                exitTop.setVisible(curr.getRoomIdentifier() == 1 || curr.getRoomIdentifier() == 2
+                        || curr.getRoomIdentifier() == 3 || curr.getRoomIdentifier() == 4
+                        || curr.getRoomIdentifier() == 6 || curr.getRoomIdentifier() == 7
+                        || curr.getRoomIdentifier() == 10);
+                exitRight.setVisible(curr.getRoomIdentifier() == 1 || curr.getRoomIdentifier() == 3
+                        || curr.getRoomIdentifier() == 4 || curr.getRoomIdentifier() == 5
+                        || curr.getRoomIdentifier() == 7 || curr.getRoomIdentifier() == 8
+                        || curr.getRoomIdentifier() == 11);
+                exitBottom.setVisible(curr.getRoomIdentifier() == 1 || curr.getRoomIdentifier() == 2
+                        || curr.getRoomIdentifier() == 4 || curr.getRoomIdentifier() == 5
+                        || curr.getRoomIdentifier() == 8 || curr.getRoomIdentifier() == 9
+                        || curr.getRoomIdentifier() == 10);
+                exitLeft.setVisible(curr.getRoomIdentifier() == 1 || curr.getRoomIdentifier() == 2
+                        || curr.getRoomIdentifier() == 3 || curr.getRoomIdentifier() == 5
+                        || curr.getRoomIdentifier() == 6 || curr.getRoomIdentifier() == 9
+                        || curr.getRoomIdentifier() == 11);
+                if(!(curr.getMonster().monsterIsDead)) {
+                    attackMonster.setVisible(true);
+                } else {
+                    attackMonster.setVisible(false);
+                }
             }
         });
-
-        // final panes and showing scene
-        primaryStage.setTitle("DungeonCrawler");
-        primaryStage.setScene(new Scene(root, bkgdWidth, bkgdHeight));
-        root.getChildren().addAll(hBox, vBox, pane, centerText);
-        return primaryStage.getScene();
     }
 
     public static void main(String[] args) {
