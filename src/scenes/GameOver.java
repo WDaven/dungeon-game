96dd3139986d7075d6d0be84309ccd435d25f210
krana8 <krana8@gatech.edu>
2021-03-27 12:47:51 -0400

    GameOver Screen + label changes

---
+++ src/scenes/GameOver.java
@@ -0,0 +1,27 @@
+package scenes;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class GameOver {
+    private static Pane background;
+    private static Label gameOver;
+    private static Button restart;
+    public static Scene start(Stage primaryStage) {
+        background = new Pane();
+        gameOver = new Label("Game Over. Please try again...");
+        gameOver.setAlignment(Pos.TOP_CENTER);
+        restart = new Button("Start Over.");
+        restart.setAlignment(Pos.CENTER);
+        background.getChildren().addAll(gameOver, restart);
+        restart.setOnAction(e -> {
+            primaryStage.setScene(InitializeConfigScreen.initConfigScreen(primaryStage));
+        });
+        return new Scene(background,400,500);
+    }
+}
+
