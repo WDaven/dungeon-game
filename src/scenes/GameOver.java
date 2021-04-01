9bc227474f379ec66cd4abf6ba1ad9307ba680d0
ebang8 <ebang8@gatech.edu>
2021-04-01 01:01:29 -0400

    fix player reset

--- src/scenes/GameOver.java
+++ src/scenes/GameOver.java
@@ -5,30 +5,26 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
+
+import static generators.Maze.getPlayer;
 
 public class GameOver {
     private static BorderPane background;
     private static Label gameOver;
     private static Button restart;
-    private static VBox goBox;
     public static Scene start(Stage primaryStage) {
         background = new BorderPane();
         gameOver = new Label("Game Over. Please try again...");
-        gameOver.setStyle("-fx-text-fill:red; -fx-font-weight:bold");
-        gameOver.setFont(new Font("verdana",20));
-        restart = new Button("Start Over");
-        goBox = new VBox();
-        goBox.getChildren().add(gameOver);
-        goBox.setAlignment(Pos.CENTER);
-        background.setTop(goBox);
+        gameOver.setAlignment(Pos.TOP_CENTER);
+        restart = new Button("Start Over.");
+        restart.setAlignment(Pos.CENTER);
+        background.setTop(gameOver);
         background.setCenter(restart);
         restart.setOnAction(e -> {
             primaryStage.setScene(InitializeConfigScreen.initConfigScreen(primaryStage));
         });
-        return new Scene(background,400,500);
+        getPlayer().setPlayerHealth(100);
+        return new Scene(background, 400, 500);
     }
 }
