285fa79dbae50f4cbaa102ee3fb840dbcd2ce65f
ebang8 <ebang8@gatech.edu>
2021-04-01 00:46:55 -0400

    Fix start over display

--- src/scenes/GameOver.java
+++ src/scenes/GameOver.java
@@ -4,26 +4,31 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
-import static generators.Maze.getPlayer;
-
 public class GameOver {
-    private static Pane background;
+    private static BorderPane background;
     private static Label gameOver;
     private static Button restart;
+    private static VBox goBox;
     public static Scene start(Stage primaryStage) {
-        background = new Pane();
+        background = new BorderPane();
         gameOver = new Label("Game Over. Please try again...");
-        gameOver.setAlignment(Pos.TOP_CENTER);
-        restart = new Button("Start Over.");
-        restart.setAlignment(Pos.CENTER);
-        background.getChildren().addAll(gameOver, restart);
+        gameOver.setStyle("-fx-text-fill:red; -fx-font-weight:bold");
+        gameOver.setFont(new Font("verdana",20));
+        restart = new Button("Start Over");
+        goBox = new VBox();
+        goBox.getChildren().add(gameOver);
+        goBox.setAlignment(Pos.CENTER);
+        background.setTop(goBox);
+        background.setCenter(restart);
         restart.setOnAction(e -> {
             primaryStage.setScene(InitializeConfigScreen.initConfigScreen(primaryStage));
         });
-        getPlayer().setPlayerHealth(100);
-        return new Scene(background, 400, 500);
+        return new Scene(background,400,500);
     }
 }
